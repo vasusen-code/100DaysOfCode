3057d82646ae8a64563778667fae8d880038c20c
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2023-05-14 22:47:38 +0530

    Update blackjack_game.py

--- blackjack_game.py
+++ blackjack_game.py
@@ -55,32 +55,33 @@ def move(set, remaining_amount, bet):
     dealer = 0
     player = 0
     next_move = input("Do you want to 'hit' or 'stand' ?").lower()
+
     if next_move == "hit":
         new_card = pick_cards(1)
-        print(f"You got a card of {new_card}.")
+        print(f"You got a card of {new_card[0]}.")
         set["player"].append(new_card[0])
         if sum(set, "player") > 21:
             print(f"You lost $ {bet}")
-            return place_bet(remaining_amount, bet)
+            remaining_amount -= bet
         else:
             move(set, remaining_amount, bet)
+
     elif next_move == "stand":
         player = sum(set, "player")
         dealer = sum(set, "dealer")
         if dealer >= 17:
             if dealer > 21:
                 print(f"Congrats, you won $ {bet}!")
-                remaining_amount += 2 * bet
-                return place_bet(remaining_amount, bet)
+                remaining_amount += bet
+                return remaining_amount
             player = 21 - player
             dealer = 21 - dealer
             if player < dealer:
                 print(f"Congrats, you won $ {bet}!")
-                remaining_amount += 2 * bet
-                return place_bet(remaining_amount, bet)
+                remaining_amount += bet
             else:
                 print(f"You lost $ {bet}")
-                return place_bet(remaining_amount, bet)
+                remaining_amount -= bet
         elif dealer == player:
             print("It is a draw.")
             remaining_amount += bet
@@ -89,35 +90,39 @@ def move(set, remaining_amount, bet):
             new_card = pick_cards(1)
             set["dealer"].append(new_card[0])
             dealer = sum(set, "dealer")
+            print(f"The dealer has a sum of {dealer}")
             if dealer > 21:
                 print(f"Congrats, you won $ {bet}!")
-                remaining_amount += 2 * bet
-                return place_bet(remaining_amount, bet)
+                remaining_amount += bet
+                return remaining_amount
             player = 21 - player
             dealer = 21 - dealer
             if player < dealer:
                 print(f"Congrats, you won $ {bet}!")
-                remaining_amount += 2 * bet
-                return place_bet(remaining_amount, bet)
+                remaining_amount += bet
             else:
                 print(f"You lost $ {bet}")
-                return place_bet(remaining_amount, bet)
+                remaining_amount -= bet
     else:
         print("Incorrect entry.")
         move(set, remaining_amount, bet)
 
-remaining_amount = 1000
+    return remaining_amount
 
+remaining_amount = 1000
+bet = 0
+    
 while not game_end:
-    bet = 0
     bet = place_bet(remaining_amount, bet)
     cards_ = pick_cards(4)
     set["dealer"] = [cards_[0], cards_[1]]
     set["player"] = [cards_[2], cards_[3]]
     print(f'''Your cards are {set["player"][0]} and {set["player"][1]},
     while the dealer has two cards out of which one is {set["dealer"][0]}''')
-    move(set, remaining_amount, bet)
+    remaining_amount = move(set, remaining_amount, bet)
     if remaining_amount == 0:
+        remaining_amount = 1000
         game_end = True
+
         
 print("You have lost all the money, restart the game.")
